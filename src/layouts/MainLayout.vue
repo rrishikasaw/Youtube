b7c1b1da610dc406f7a955ddc68d7b6d4cecd726
rrishikasaw <rrishikasaw@gmail.com>
2024-03-27 17:14:01 +0530

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,154 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated class="header">
+      <q-toolbar class="flex justify-between">
+        <div class="flex">
+          <q-btn
+            flat
+            dense
+            round
+            icon="menu"
+            aria-label="Menu"
+            @click="toggleLeftDrawer"
+          />
+
+          <q-toolbar-title class="flex">
+            <img src="../assets/image/home/youtube.jpg" width="130" alt="" />
+          </q-toolbar-title>
+        </div>
+
+        <div class="flex hide-sm">
+         
+
+          <q-input
+  outlined
+  v-model="searchText"
+  placeholder="Search..."
+  clearable
+  dense
+  rounded
+  style="width: 500px"
+ 
+>
+  <template v-slot:prepend>
+    <q-icon name="search" class="cursor-pointer" />
+  </template>
+  <template v-slot:append>
+    <q-icon name="event" class="cursor-pointer" />
+  </template>
+</q-input>
+          
+
+           <q-icon
+          slot="append"
+          name="mic"
+          class="cursor-pointer q-mx-md  q-mt-sm"
+          @click="startVoiceSearch"
+          size="28px"
+        />
+        </div>
+       
+
+       <div class="flex hide-sm">
+         <q-icon
+          slot="append"
+          name="more_vert"
+          class="cursor-pointer q-mr-sm q-mt-sm"
+          @click="openMenu"
+          size="24px"
+        />
+        
+    <q-btn outline rounded color="primary" label="Profile"  icon="person" />
+    <q-icon
+  slot="append"
+  name="water_drop"
+  class="cursor-pointer q-mx-md q-mt-sm"
+  @click="openMenu"
+  size="24px"
+/>
+       </div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <q-list>
+        <q-item-label header> Essential Links </q-item-label>
+
+        <EssentialLink
+          v-for="link in linksList"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import EssentialLink from "components/EssentialLink.vue";
+
+defineOptions({
+  name: "MainLayout",
+});
+
+const linksList = [
+  {
+    title: "Home",
+       // caption: "github.com/quasarframework",
+    icon: "home",
+    link: "https://quasar.dev",
+  },
+  {
+    title: "Shorts",
+    // caption: "github.com/quasarframework",
+    icon: "code",
+    link: "https://github.com/quasarframework",
+  },
+  {
+    title: "video",
+    // caption: "chat.quasar.dev",
+    icon: "chat",
+    link: "https://chat.quasar.dev",
+  },
+ 
+  {
+    title: "Save",
+    // caption: "@quasarframework",
+    icon: "rss_feed",
+    link: "https://twitter.quasar.dev",
+  },
+  {
+    title: "Download",
+    // caption: "@QuasarFramework",
+    icon: "public",
+    link: "https://facebook.quasar.dev",
+  },
+ 
+];
+
+const leftDrawerOpen = ref(false);
+
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
+}
+</script>
+
+<style scoped>
+
+
+
+    @media (max-width: 800px) {
+      .hide-sm {
+        display: none;
+      }
+    }
+.header {
+  background-color: white;
+  color: black;
+}
+</style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,111 @@
+<template>
+  <q-page class="flex ">
+    <section class="first-section q-mx-auto">
+   
+      <div class="flex justify-evenly flex-wrap  q-mx-auto service ">
+        <q-card class="card">
+          <img src="../assets/image/home/image1.jpg" width="300" alt="" />
+         <div class="q-px-md">
+           <h5 class="font-bold q-my-none q-mt-sm">Nature</h5>
+          <p class="q-mb-none">Lorem ipsum dolor</p>
+          <p>₹100</p>
+         </div>
+        </q-card>
+
+             <q-card class="card">
+          <img src="../assets/image/home/mountains.jpg" width="300" alt="" />
+           <div class="q-px-md">
+           <h5 class="font-bold q-my-none q-mt-sm">Nature</h5>
+          <p class="q-mb-none">Lorem ipsum dolor</p>
+          <p>₹100</p>
+         </div>
+        </q-card>
+
+             <q-card class="card">
+          <img src="../assets/image/home/image1.jpg" width="300" alt="" />
+        <div class="q-px-md">
+           <h5 class="font-bold q-my-none q-mt-sm">Nature</h5>
+          <p class="q-mb-none">Lorem ipsum dolor</p>
+          <p>₹100</p>
+         </div>
+        </q-card>
+
+             <q-card class="card">
+          <img src="../assets/image/home/mountains.jpg" width="300" alt="" />
+        <div class="q-px-md">
+           <h5 class="font-bold q-my-none q-mt-sm">Nature</h5>
+          <p class="q-mb-none">Lorem ipsum dolor</p>
+          <p>₹100</p>
+         </div>
+        </q-card>
+
+            <q-card class="card">
+          <img src="../assets/image/home/image1.jpg" width="300" alt="" />
+          <div class="q-px-md">
+           <h5 class="font-bold q-my-none q-mt-sm">Nature</h5>
+          <p class="q-mb-none">Lorem ipsum dolor</p>
+          <p>₹100</p>
+         </div>
+        </q-card>
+
+              <q-card class="card">
+          <img src="../assets/image/home/mountains.jpg" width="300" alt="" />
+          <div class="q-px-md">
+           <h5 class="font-bold q-my-none q-mt-sm">Nature</h5>
+          <p class="q-mb-none">Lorem ipsum dolor</p>
+          <p>₹100</p>
+         </div>
+        </q-card>
+           
+
+            <q-card class="card">
+          <img src="../assets/image/home/image1.jpg" width="300" alt="" />
+          <div class="q-px-md">
+           <h5 class="font-bold q-my-none q-mt-sm">Nature</h5>
+          <p class="q-mb-none">Lorem ipsum dolor</p>
+          <p>₹100</p>
+         </div>
+        </q-card>
+
+            <q-card class="card">
+          <img src="../assets/image/home/mountains.jpg" width="300" alt="" />
+        <div class="q-px-md">
+           <h5 class="font-bold q-my-none q-mt-sm">Nature</h5>
+          <p class="q-mb-none">Lorem ipsum dolor</p>
+          <p>₹100</p>
+         </div>
+        </q-card>
+      </div>
+    </section>
+  </q-page>
+</template>
+
+<script setup>
+defineOptions({
+  name: "IndexPage",
+});
+</script>
+
+<style scoped>
+.first-section {
+  /* padding-inline: 30px; */
+  margin-top: 50px;
+  margin-bottom: 30px;
+}
+
+.service {
+  max-width: 1500px;
+}
+
+.card3 {
+  max-width: 300px;
+  padding: 10px;
+  height: 400px;
+}
+
+.card{
+  max-width:300px ;
+  margin-inline: 10px;
+  margin-top: 15px;
+}
+</style>
